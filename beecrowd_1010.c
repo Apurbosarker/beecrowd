61da823e3c2f92f4940f64f46c88c7b6c362b062
Apurbo <email@example.com>
2022-09-12 02:28:04 +0600

    done

--- beecrowd_1010.c
+++ beecrowd_1010.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int c1, c2, u1, u2;
+    int C1, C2, u1, u2;
     float p1, p2, total;
-    scanf("%d%d%f", &c1, &u1, &p1);
-    scanf("%d%d%f", &c2, &u2, &p2);
+    scanf("%d%d%f", &C1, &u1, &p1);
+    scanf("%d%d%f", &C2, &u2, &p2);
     total = (u1 * p1) + (u2 * p2);
     printf("VALOR A PAGAR: R$ %.2lf\n", total);
     return 0;
