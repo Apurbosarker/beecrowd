20aace63a72a05aaf3f3bd99c6e13803488de8df
Apurbo <email@example.com>
2022-09-23 01:18:20 +0600

    update

--- beecrowd_1011.c
+++ beecrowd_1011.c
@@ -2,9 +2,9 @@
 int main()
 {
     int r;
-    float ans;
+    double ans;
     scanf("%d", &r);
     ans = (4 / 3.0) * 3.14159 * r * r * r;
-    printf("VOLUME = %.3f\n",ans);
+    printf("VOLUME = %.3lf\n",ans);
     return 0;
 }
